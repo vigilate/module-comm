8f9a731c45f8d00a06d0c631356e4d257bd31d35
fr0g <manuel.poncet@epitech.eu>
2015-10-03 17:37:39 +0000

    Can now try various db types

--- vigilate_db.py
+++ vigilate_db.py
@@ -4,12 +4,18 @@ class vigilate_db:
     """VIGILATE's dabatase communication module"""
 
     def __init__(self):
+        self.connected = False
 
+        self.connect("mysql", "root", "toor")
+        if not self.connected:
+            self.connect("postgres", "root", "toor")
+
+    def connect(self, dbtype, user, password, dbname="vigilate", host="localhost"):
         try:
-            self.e = engine.create_engine("mysql://root:toor@localhost/vigilate")
+            self.e = engine.create_engine("%s://%s:%s@%s/%s" % (dbtype, user, password, host, dbname))
             connection = self.e.connect()
         except exc.OperationalError as err:
             print (err)
         else:
-            print ("Successfully connected to mariadb database.")
-            return
+            print ("Successfully connected to the %s database." % dbtype)
+            self.connected = True
