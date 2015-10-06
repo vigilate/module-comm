a98ef970292b61617a7b562dd644382d462eb57a
fr0g <manuel.poncet@epitech.eu>
2015-10-06 16:28:36 +0000

    vigilate_db class removed, now use a single function

--- vigilate_db.py
+++ vigilate_db.py
@@ -1,23 +1,29 @@
 from sqlalchemy import engine, exc
 from sqlalchemy.orm import sessionmaker 
-from table_declaration import Dealer, Users, User_monitored_program, User_preference, Vulns
+from table_declaration import Users, User_monitored_program, User_preference, Vulns
 
-class vigilate_db:
-    """VIGILATE's dabatase communication module"""
+def connect_to_database():
+    try:
+        e = engine.create_engine("mysql+pymysql://root:toor@localhost/vigilate")
+        connection = e.connect()
+    except exc.OperationalError as err:
+        print (err)
+    else:
+        print ("Successfully connected to the mysql database.")
+        session = sessionmaker(bind=e)
+        return session()
 
-    def __init__(self):
-        self.connected = False
+    try:
+        e = engine.create_engine("postgresql://root:toor@localhost/vigilate")
+        connection = e.connect()
+    except exc.OperationalError as err:
+        print (err)
+    else:
+        print ("Successfully connected to the postgresql database")
+        session = sessionmaker(bind=e)
+        return session()
 
-        self.connect("mysql+pymysql", "root", "toor")
-        if not self.connected:
-            self.connect("postgresql", "root", "toor")
-
-    def connect(self, dbtype, user, password, dbname="vigilate", host="localhost"):
-        try:
-            self.e = engine.create_engine("%s://%s:%s@%s/%s" % (dbtype, user, password, host, dbname))
-            connection = self.e.connect()
-        except exc.OperationalError as err:
-            print (err)
-        else:
-            print ("Successfully connected to the %s database." % dbtype)
-            self.connected = True
+def add_user(session, username, email, password, id_preference, user_type, contrat):
+    new_user = Users(username=username, email=email, password=password, type=user_type, contrat=contrat)
+    session.add(new_user)
+    session.commit()
