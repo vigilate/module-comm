0257d34de74000ecb336694b8dd3367528c94483
ampotos <mercie_i@epitech.eu>
2015-10-06 14:33:25 +0100

    add foreign key and replace the dealer table by a id_dealer field in the user

--- table_declaration.py
+++ table_declaration.py
@@ -5,14 +5,6 @@ from sqlalchemy_utils.functions import database_exists, create_database
 from sqlalchemy import create_engine, exc
 
 Base = declarative_base()
-
-class Dealer(Base):
-    __tablename__ = 'dealer'
-
-    id = Column(Integer, primary_key=True)
-    id_user_dealer = Column(Integer, ForeignKey('users.id'), nullable=False)
-    id_user_client = Column(Integer, ForeignKey('users.id'), nullable=False)
-
     
 class Users(Base):
     __tablename__ = 'users'
@@ -21,10 +13,9 @@ class Users(Base):
     username = Column(String(20), nullable=False, unique=True)
     email = Column(String(100), nullable=False, unique=True)
     password = Column(String(60), nullable=False)
-    id_preference = Column(Integer, nullable=False)
     type = Column(Integer, nullable=False)
     contrat = Column(Integer, nullable=False)
-
+    id_dealer = Column(Integer, ForeignKey('users.id'))
     
 class User_monitored_program(Base):
     __tablename__ = 'user_monitored_program'
@@ -33,15 +24,15 @@ class User_monitored_program(Base):
     program_name = Column(String(20), nullable=False)
     program_version = Column(String(20), nullable=False)
     minimum_score = Column(Integer, nullable=False)
-    id_user = Column(Integer, nullable=False)
+    id_user = Column(Integer, ForeignKey('users.id'), nullable=False)
 
     
 class User_preference(Base):
     __tablename__ = 'user_preference'
 
     id = Column(Integer, primary_key=True)
-    id_user = Column(Integer, nullable=False)
-    id_prgram = Column(Integer, nullable=False)
+    id_user = Column(Integer, ForeignKey('users.id'), nullable=False)
+    id_program = Column(Integer, ForeignKey('user_monitored_program.id'), nullable=False)
     score = Column(Integer, nullable=False)
     alert_mean = Column(Integer, nullable=False)
 
@@ -58,12 +49,13 @@ class Vulns(Base):
     simplified_detail = Column(Text, nullable=False)
     score = Column(Integer, nullable=False)
 
+    
 class Alertes(Base):
     __tablename__ = 'alertes'
 
     id = Column(Integer, primary_key=True)
-    id_user = Column(Integer, nullable=False)
-    id_cve = Column(Integer, nullable=False)
+    id_user = Column(Integer, ForeignKey('users.id'), nullable=False)
+    id_cve = Column(Integer, ForeignKey('vulns.id'), nullable=False)
     alert_mean = Column(Integer, nullable=False)
 
 print("Setup the data base")
