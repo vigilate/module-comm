1e4d5936999b028715063d4be1ec93259b602424
Kevin Soules <inceax64@gmail.com>
2015-10-06 14:50:21 +0100

    Separte creation and table

--- table_declaration.py
+++ table_declaration.py
@@ -1,62 +1,6 @@
-from sqlalchemy import Column, ForeignKey, Integer, String, Date, Text
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import relationship, sessionmaker
 from sqlalchemy_utils.functions import database_exists, create_database
 from sqlalchemy import create_engine, exc
-
-Base = declarative_base()
-    
-class Users(Base):
-    __tablename__ = 'users'
-
-    id = Column(Integer, primary_key=True)
-    username = Column(String(20), nullable=False, unique=True)
-    email = Column(String(100), nullable=False, unique=True)
-    password = Column(String(60), nullable=False)
-    type = Column(Integer, nullable=False)
-    contrat = Column(Integer, nullable=False)
-    id_dealer = Column(Integer, ForeignKey('users.id'))
-    
-class User_monitored_program(Base):
-    __tablename__ = 'user_monitored_program'
-
-    id = Column(Integer, primary_key=True)
-    program_name = Column(String(20), nullable=False)
-    program_version = Column(String(20), nullable=False)
-    minimum_score = Column(Integer, nullable=False)
-    id_user = Column(Integer, ForeignKey('users.id'), nullable=False)
-
-    
-class User_preference(Base):
-    __tablename__ = 'user_preference'
-
-    id = Column(Integer, primary_key=True)
-    id_user = Column(Integer, ForeignKey('users.id'), nullable=False)
-    id_program = Column(Integer, ForeignKey('user_monitored_program.id'), nullable=False)
-    score = Column(Integer, nullable=False)
-    alert_mean = Column(Integer, nullable=False)
-
-
-class Vulns(Base):
-    __tablename__ = 'vulns'
-
-    id = Column(Integer, primary_key=True)
-    cveid = Column(String(20), nullable=False)
-    program_name = Column(String(100), nullable=False)
-    program_version = Column(String(100), nullable=False)
-    date = Column(Date, nullable=False)
-    detail = Column(Text, nullable=False)
-    simplified_detail = Column(Text, nullable=False)
-    score = Column(Integer, nullable=False)
-
-    
-class Alertes(Base):
-    __tablename__ = 'alertes'
-
-    id = Column(Integer, primary_key=True)
-    id_user = Column(Integer, ForeignKey('users.id'), nullable=False)
-    id_cve = Column(Integer, ForeignKey('vulns.id'), nullable=False)
-    alert_mean = Column(Integer, nullable=False)
+from Table import *
 
 print("Setup the data base")
 if database_exists('mysql+pymysql://root:toor@localhost/vigilate') or database_exists('postgresql://vigilate@localhost/vigilate'):
