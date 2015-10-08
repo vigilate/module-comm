1206dbc7307fa5bdebebd0e89f5e046586b3424b
Kevin Soules <inceax64@gmail.com>
2015-10-08 19:12:23 +0100

    Start adding config support

---
+++ Config.py
@@ -0,0 +1,3 @@
+from configure import Configuration
+
+config = Configuration.from_file('./vigilate.conf').configure()
--- table_declaration.py
+++ table_declaration.py
@@ -1,18 +1,27 @@
 from sqlalchemy_utils.functions import database_exists, create_database
 from sqlalchemy import create_engine, exc
 from Table import *
+import Config
 
 print("Setup the data base")
-if database_exists('mysql+pymysql://root:toor@localhost/vigilate') or database_exists('postgresql://vigilate@localhost/vigilate'):
+mysql_url = 'mysql+pymysql://%s:%s@%s/vigilate' % (Config.config.mysql_user,
+                                                   Config.config.mysql_pwd,
+                                                   Config.config.pgsql_host)
+
+pgsql_url = 'postgresql://%s:%s@%s/vigilate' % (Config.config.pgsql_user,
+                                                Config.config.pgsql_pwd,
+                                                Config.config.pgsql_host)
+
+if database_exists(mysql_url) or database_exists(pgsql_url):
     print ("Database already setup")
 else:
     try:
-        create_database('mysql+pymysql://root:toor@localhost/vigilate')
-        engine = create_engine('mysql+pymysql://root:toor@localhost/vigilate')
+        create_database(mysql_url)
+        engine = create_engine(mysql_url)
     except exc.OperationalError as err1:
         try:
-            create_database('postgresql://vigilate@localhost/vigilate')
-            engine = create_engine('postgresql://vigilate@localhost/vigilate')
+            create_database(pgsql_url)
+            engine = create_engine(pgsql_url)
         except exc.OperationalError as err2:
             exit("Can't create database : mysql, postgres %s" % err2)
             
