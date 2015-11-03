3de617699016c591a5fdbbdd44f3620876a93eeb
Kevin Soules <inceax64@gmail.com>
2015-11-03 18:01:25 +0000

    Fix compile error because of missing -L

--- PyArgon2/setup.py
+++ PyArgon2/setup.py
@@ -4,6 +4,6 @@ from distutils.core import setup, Extension
 setup(name='PyArgon2', version='1.0',
       ext_modules=[Extension('PyArgon2',
                              sources = ['PyArgon2.c'],
-                             library_dirs=['/usr/lib'],
+                             library_dirs=['/usr/lib', '.'],
                              libraries=['argon2'],
                              include_dirs=['.'])])
